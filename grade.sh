97ffb92f5689204bf2f11933c63d975e2ab85569
Profx5 <156368829+Profx5@users.noreply.github.com>
2024-02-15 19:13:41 -0800

    Update

--- grade.sh
+++ grade.sh
@@ -32,6 +32,12 @@ fi
 
 echo "Program compiled successfully"
 
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > Test-output.txt
+
+# lastLine= $(cat Test-output.txt | tail -n 2| awk '{print $4}')
+cat Test-output.txt | tail -n 2| head -n 1| awk -F'[, ]' '{print $3}'
+cat Test-output.txt | tail -n 2| awk '{print $6}'
+
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
