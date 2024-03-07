231bd4eb3b56de0101925cd1c6303892d69e3cb2
Profx5 <156368829+Profx5@users.noreply.github.com>
2024-03-07 12:33:44 -0800

    We actually did it

--- grade.sh
+++ grade.sh
@@ -32,11 +32,18 @@ fi
 
 echo "Program compiled successfully"
 
-java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > Test-output.txt
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > Test-output.txt
 
 # lastLine= $(cat Test-output.txt | tail -n 2| awk '{print $4}')
-cat Test-output.txt | tail -n 2| head -n 1| awk -F'[, ]' '{print $3}'
-cat Test-output.txt | tail -n 2| awk '{print $6}'
+tests=$(cat Test-output.txt | tail -n 2| head -n 1| awk -F'[, ]' '{print $3}')
+failures=$(cat Test-output.txt | tail -n 2| awk '{print $5}')
+successes=$(($tests - $failures))
+
+echo "Your score is $successes / $tests"
+
+# echo tests
+# echo failures
+# echo "Your score is $successes/$tests"
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
